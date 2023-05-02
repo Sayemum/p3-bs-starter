7f8dfc72930401992820973ef8dc4bd7874926e6
Sayemum "Simon" Hassan <84354582+Sayemum@users.noreply.github.com>
2023-05-02 13:54:13 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,38 @@
-const searchBox = document.getElementById('top-search')
-searchBox.onsubmit = (ev) => {
-  console.log('submitted top-search with', ev)
-  // https://stackoverflow.com/a/26892365/1449799
-  const formData = new FormData(ev.target)
-  console.log(formData)
-  for (const pair of formData.entries()) {
-    console.log(`${pair[0]}, ${pair[1]}`);
-  }
-  ev.preventDefault()
+function displayGameProfile(game) {
+  // Add code to display game profile information (title, release year, etc.)
+  // in the "gameProfile" div.
+  console.log("DISPLAYING GAME PROFILE: ", game)
 }
+
+function displayGameImages(images) {
+  // Add code to display game images in the "gameImages" div.
+  console.log("DISPLAYING GAME IMAGES: ", images)
+}
+
+function displayGameNews(news) {
+  // Add code to display game news articles in the "gameNews" div.
+  console.log("DISPLAYING GAME NEWS: ", news)
+}
+
+document.getElementById("searchForm").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const searchInput = document.getElementById("searchInput");
+  const query = searchInput.value.trim();
+
+  if (query) {
+    // Search for video games using the RAWG API
+    const results = await searchVideoGames(query);
+    const game = results[0]; // Display the first result, or implement code to let the user choose from multiple results
+
+    // Display game profile information
+    displayGameProfile(game);
+
+    // Search for game images using the Web Search API
+    const images = await searchGameImages(query);
+    displayGameImages(images);
+
+    // Search for game news using the Web Search API
+    const news = await searchGameNews(query);
+    displayGameNews(news);
+  }
+});
